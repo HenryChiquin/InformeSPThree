2334177b4a73fe1fe7c2607b628ebf0b7e6e3f4d
HenryChiquin <chiquinhenry888@gmail.com>
2023-06-16 19:59:14 -0600

    Agregar archivos de proyecto.

---
+++ InformeSPThree/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.crvReportes = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.SuspendLayout()
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBuscar.Location = New System.Drawing.Point(643, 26)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(57, 21)
+        Me.btnBuscar.TabIndex = 0
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(152, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Ingrese el criterio de busqueda"
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtBuscar.Location = New System.Drawing.Point(186, 28)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(433, 20)
+        Me.txtBuscar.TabIndex = 2
+        '
+        'crvReportes
+        '
+        Me.crvReportes.ActiveViewIndex = -1
+        Me.crvReportes.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.crvReportes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.crvReportes.Cursor = System.Windows.Forms.Cursors.Default
+        Me.crvReportes.Location = New System.Drawing.Point(31, 97)
+        Me.crvReportes.Name = "crvReportes"
+        Me.crvReportes.ShowCloseButton = False
+        Me.crvReportes.ShowGroupTreeButton = False
+        Me.crvReportes.ShowLogo = False
+        Me.crvReportes.ShowParameterPanelButton = False
+        Me.crvReportes.Size = New System.Drawing.Size(669, 467)
+        Me.crvReportes.TabIndex = 3
+        Me.crvReportes.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(750, 576)
+        Me.Controls.Add(Me.crvReportes)
+        Me.Controls.Add(Me.txtBuscar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents crvReportes As CrystalDecisions.Windows.Forms.CrystalReportViewer
+End Class
---
+++ InformeSPThree/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CargarReporte()
+        Dim oReporte As New SPprueba
+        oReporte.DataSourceConnections.Item(0).SetLogon("sa", "system")
+        oReporte.SetParameterValue("@texto", txtBuscar.Text)
+        Me.crvReportes.ReportSource = oReporte
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        CargarReporte()
+    End Sub
+End Class
